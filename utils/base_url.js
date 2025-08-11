90c3821dcbc7617e2648f948c476fcc94ecb19c4
Anurag <anuragsonkar053@gmail.com>
2025-08-11 20:05:07 +0530

    base-url

--- utils/base_url.js
+++ utils/base_url.js
@@ -1,2 +1,3 @@
-export const base_url = 'http://localhost:8000'
+// export const base_url = 'http://localhost:8000'
+export const base_url = 'https://profopedia-backend-dg6y.onrender.com'
 // export const base_url = 'https://profopedia-backend.onrender.com'
